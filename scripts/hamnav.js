c6cc1f3e44535b8b7de6eedbc5489339f66699dc
terdeck <taeraesuu@gmail.com>
2024-01-27 15:06:58 -0500

    revamp css files

--- scripts/hamnav.js
+++ scripts/hamnav.js
@@ -4,6 +4,6 @@ const navigation = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menu');
 
 hamButton.addEventListener('click', () => {
-	navigation.classlist.toggle('show');
+	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
