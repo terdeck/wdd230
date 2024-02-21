8439789bc5e77d34c4589439828d27848ffc4eb5
terdeck <taeraesuu@gmail.com>
2024-02-21 18:43:53 -0500

    update wk7 localStorage

--- lesson07/scripts/bom.js
+++ lesson07/scripts/bom.js
@@ -4,22 +4,44 @@ const input = document.querySelector('#favChap');
 const button = document.querySelector('button');
 const list = document.querySelector("#list");
 
-button.addEventListener('click', () => {
-   if (input.value.trim() != "") {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        // deleteButton.style.marginLeft = '25px';
-        li.appendChild(deleteButton);
-        list.appendChild(li);
+let chaptersArray = getChapterList() || [];
 
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
-            input.focus();
-        });
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function displayList(item) {
+    let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    li.textContent = item;
+    deleteButton.textContent = "❌"
+    deleteButton.classList.add("delete");
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener("click", function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
         input.focus();
-        input.value = '';
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem("favBOMList", JSON.stringify(chaptersArray));
+}
+
+function getChapterList(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
+
+button.addEventListener('click', () => {
+   if (input.value != "") {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
     }
    else {
     alert('Please enter a value')
