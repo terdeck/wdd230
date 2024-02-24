811a96aefb3c087f9be36fcfdaf2f1c82587c725
terdeck <taeraesuu@gmail.com>
2024-02-24 13:01:04 -0500

    update chamber wk7

--- chamber/scripts/hamnav.js
+++ chamber/scripts/hamnav.js
@@ -7,3 +7,13 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("discover-main").style.marginLeft = "250px";
+  }
+  
+function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("discover-main").style.marginLeft= "0";
+  }
