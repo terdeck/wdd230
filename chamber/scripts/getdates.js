2628fb1ec978f0e659460559fa4d4e72cd284373
terdeck <taeraesuu@gmail.com>
2024-03-16 13:41:21 -0400

    update wk10 chamber banner

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,4 +1,4 @@
-// chamber getdates
+// chamber getdates & banner
 
 document.addEventListener("DOMContentLoaded", function () {
     // Set current year for copyright
@@ -17,3 +17,15 @@ document.addEventListener("DOMContentLoaded", function() {
       timestampField.value = Date.now();
     }
   });
+
+  
+// banner
+const currentDate = new Date();
+const currentDay = currentDate.getDay();
+
+if (currentDay >= 1 && currentDay <= 3) {
+  document.getElementById("banner").style.display = "block";
+}
+else {
+  document.getElementById("banner").style.display = "none"
+}
