eb9137a1c9ab627f4f8e52e8efea82c01cb3cd14
terdeck <taeraesuu@gmail.com>
2024-03-18 13:59:03 -0400

    update chamber

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -2,9 +2,17 @@
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
+const banner = document.getElementById("banner");
+
 if (currentDay >= 1 && currentDay <= 3) {
-  document.getElementById("banner").style.display = "block";
+  banner.style.display = "block";
 }
 else {
-  document.getElementById("banner").style.display = "none"
+  banner.style.display = "none";
 }
+
+const bannerClose = document.querySelector(".banner-close");
+
+bannerClose.addEventListener('click', () => {
+	banner.style.display = "none";
+});
