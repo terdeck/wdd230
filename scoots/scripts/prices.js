ee0a2f970138e7ae4969416f399d632a38129a32
terdeck <taeraesuu@gmail.com>
2024-04-04 13:24:54 -0400

    update scoots rental table

--- scoots/scripts/prices.js
+++ scoots/scripts/prices.js
@@ -1,90 +1,81 @@
 const linksURL = "https://terdeck.github.io/wdd230/scoots/data/prices.json";
-const prices = document.querySelector("#pricing-table");
 
 async function getPrices(){
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
-    displayMembers(data.prices);
+    priceTable(data.prices);
 } 
 
-const priceTable = (prices) => {
-    // let table = document.createElement("table");
-    // let tableTitle = document.createElement("thead");
+function priceTable(data) {
+    const tableContainer = document.getElementById("table-container");
+    const table = document.createElement("table");
 
-    // let titleRow1 = document.createElement("tr");
-    // let titleRow2 = document.createElement("tr");
-    // let titleRow3 = document.createElement("tr");
+    let tableTitle = table.insertRow();
+    let title = document.createElement("th");
+    title.textContent = "Rental Pricing";
+    title.setAttribute("colspan", "6");
+    title.setAttribute("scope", "colgroup");
+    tableTitle.appendChild(title);
+    
+    let titleRow1 = table.insertRow();
+    let tableHeading1 = document.createElement("th");
+    tableHeading1.textContent = "&nbsp";
+    const tableHeading2 = document.createElement("th");
+    tableHeading2.textContent = "Reservation";
+    const tableHeading3 = document.createElement("th");
+    tableHeading3.textContent = "Walk-In";
+    tableHeading1.setAttribute("colspan", "2");
+    tableHeading1.setAttribute("scope", "colgroup");
+    tableHeading2.setAttribute("colspan", "2");
+    tableHeading2.setAttribute("scope", "colgroup");
+    tableHeading3.setAttribute("colspan", "2");
+    tableHeading3.setAttribute("scope", "colgroup");
+    titleRow1.appendChild(tableHeading1);
+    titleRow1.appendChild(tableHeading2);
+    titleRow1.appendChild(tableHeading3);
 
-    // let tableHeading1 = document.createElement("th");
-    // let tableHeading2 = document.createElement("th");
-    // let tableHeading3 = document.createElement("th");
-    // let tableHeading4 = document.createElement("th");
-    // let tableHeading5 = document.createElement("th");
-    // let tableHeading6 = document.createElement("th");
-    // let tableHeading7 = document.createElement("th");
-    // let tableHeading8 = document.createElement("th");
-    // let tableHeading9 = document.createElement("th");
-    // let tableHeading10 = document.createElement("th");
+    let titleRow2 = table.insertRow();
+    let tableHeading4 = document.createElement("th");
+    tableHeading4.textContent = "Rental Type";
+    let tableHeading5 = document.createElement("th");
+    tableHeading5.textContent = "Max Occupants";
+    let tableHeading6 = document.createElement("th");
+    tableHeading6.textContent = "Half Day";
+    let tableHeading7 = document.createElement("th");
+    tableHeading7.textContent = "Full Day";
+    let tableHeading8 = document.createElement("th");
+    tableHeading8.textContent = "Half Day";
+    let tableHeading9 = document.createElement("th");
+    tableHeading9.textContent = "Full Day";
+    tableHeading4.setAttribute("scope", "col");
+    tableHeading5.setAttribute("scope", "col");
+    tableHeading6.setAttribute("scope", "col");
+    tableHeading7.setAttribute("scope", "col");
+    tableHeading8.setAttribute("scope", "col");
+    tableHeading9.setAttribute("scope", "col");
+    titleRow2.appendChild(tableHeading4);
+    titleRow2.appendChild(tableHeading5);
+    titleRow2.appendChild(tableHeading6);
+    titleRow2.appendChild(tableHeading7);
+    titleRow2.appendChild(tableHeading8);
+    titleRow2.appendChild(tableHeading9);
 
-    // let tableBody = document.createElement("tbody");
-
-    // let tableRow1 = document.createElement("tr");
-    // let tableRow2 = document.createElement("tr");
-    // let tableRow3 = document.createElement("tr");
-    // let tableRow4 = document.createElement("tr");
-    // let tableRow5 = document.createElement("tr");
-    // let tableRow6 = document.createElement("tr");
-
-    // let tableData1 = document.createElement("td");
-    // let tableData2 = document.createElement("td");
-    // let tableData3 = document.createElement("td");
-    // let tableData4 = document.createElement("td");
-    // let tableData5 = document.createElement("td");
-    // let tableData6 = document.createElement("td");
-    // let tableData7 = document.createElement("td");
-    // let tableData8 = document.createElement("td");
-    // let tableData9 = document.createElement("td");
-    // let tableData10 = document.createElement("td");
-    // let tableData11 = document.createElement("td");
-    // let tableData12 = document.createElement("td");
-    // let tableData13 = document.createElement("td");
-    // let tableData14 = document.createElement("td");
-    // let tableData15 = document.createElement("td");
-    // let tableData16 = document.createElement("td");
-    // let tableData17 = document.createElement("td");
-    // let tableData18 = document.createElement("td");
-    // let tableData19 = document.createElement("td");
-    // let tableData20 = document.createElement("td");
-    // let tableData21 = document.createElement("td");
-    // let tableData22 = document.createElement("td");
-    // let tableData23 = document.createElement("td");
-    // let tableData24 = document.createElement("td");
-    // let tableData25 = document.createElement("td");
-    // let tableData26 = document.createElement("td");
-    // let tableData27 = document.createElement("td");
-    // let tableData28 = document.createElement("td");
-    // let tableData29 = document.createElement("td");
-    // let tableData30 = document.createElement("td");
-    // let tableData31 = document.createElement("td");
-    // let tableData32 = document.createElement("td");
-    // let tableData33 = document.createElement("td");
-    // let tableData34 = document.createElement("td");
-    // let tableData35 = document.createElement("td");
-    // let tableData36 = document.createElement("td");
-
-    prices.forEach((price) => {
-        let model = document.createElement();
-        let maxOccupants = document.createElement();
-        let reserveHalf = document.createElement();
-        let reserveFull = document.createElement();
-        let walkHalf = document.createElement();
-        let walkFull = document.createElement();
-
-
-
-
-    }),
+    data.forEach(item => {
+        const row = table.insertRow();
+        for (const key in item)
+    })
+    
+    tableContainer.appendChild(table);
 }
 
 getPrices();
+
+
+// prices.forEach((price) => {
+//     let model = document.createElement();
+//     let maxOccupants = document.createElement();
+//     let reserveHalf = document.createElement();
+//     let reserveFull = document.createElement();
+//     let walkHalf = document.createElement();
+//     let walkFull = document.createElement();
